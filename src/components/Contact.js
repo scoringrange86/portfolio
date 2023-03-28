5fd4f133263d622e9e9b3d99043344abb36077c9
Steve <scoringrange86@gmail.com>
2023-03-28 09:57:30 -0400

    Updated button styling and tab icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,41 +11,29 @@ function Contact() {
                         { name: "Github", path: "https://github.com/scoringrange86"},
                          {name: "Email", path: "mailto:scoringrange86@gmail.com"}
                     ]
-
-
     return (
         <div className="contact-wrapper">
             <h1> Contact </h1>
             <div className="contact-content-wrapper">
-                <div className="contact-content"> 
-                {
-                    contacts.map((contact) => (
-                            <a 
-                            className="contact-item"
-                             key={contact.name}
-                            href={contact.path}
-                            rel= "noopener noreferrer" 
-                            target="_blank">
-                                <p>{contact.name}</p>
-                            </a>
+                <table>
+                    {
+                    contacts.map( (contact) => (
+                        <tr>
+                            <td>
+                                <a href={contact.path}>
+                                 {contact.name}
+                                </a>
+                           </td>
+                            <td>
+                                <a href={contact.path}>
+                                {contact.path}
+                                </a>
+                                </td>
+                        </tr>
+                    
                     ))
-                }
-                </div>
-                <div className="contact-content">
-                {
-                    contacts.map((actualLink) => (
-                            <a
-                            className="contact-item" 
-                            key={actualLink.path}
-                            href={actualLink.path}
-                            rel="noreferrer"
-                                target="_blank"
-                            >
-                                <p>{actualLink.path}</p>
-                            </a>
-                    ))
-                }
-                </div>
+                    }
+                </table>
                 </div>
         </div>
 
